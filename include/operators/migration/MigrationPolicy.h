6f4bc10d1cafe070dbcc2a45248a116fdfb16a21
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2026-03-24 02:14:20 +0200

    Structure for the island model GA implementation implemented, topology interface and concrete classes implemented

---
+++ include/operators/migration/MigrationPolicy.h
@@ -0,0 +1,16 @@
+#ifndef MIGRATION_POLICY_H
+#define MIGRATION_POLICY_H
+
+#pragma once
+
+namespace galib {
+
+    class MigrationPolicy {
+    public:
+        virtual ~MigrationPolicy() = default;
+
+    };
+
+}
+
+#endif // MIGRATION_POLICY_H
