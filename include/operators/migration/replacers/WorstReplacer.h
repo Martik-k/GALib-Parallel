6c23b1c409fc8e867a53e6e7ae969547e7ef8e8d
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2026-03-24 04:21:19 +0200

    Selector and Replacer operators implemented, spaceship operator added to the Individual, minor changes in Population

---
+++ include/operators/migration/replacers/WorstReplacer.h
@@ -0,0 +1,42 @@
+#ifndef WORST_REPLACER_H
+#define WORST_REPLACER_H
+
+#pragma once
+
+#include "operators/migration/replacers/DemeReplacer.h"
+#include "core/Individual.h"
+#include "core/Population.h"
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+namespace galib {
+
+    template<typename GeneType>
+    class WorstReplacer : public DemeReplacer<GeneType> {
+    public:
+        void replaceDeme(
+            Population<GeneType>& population,
+            const std::vector<Individual<GeneType>>& deme
+        ) const override {
+            const std::size_t count = deme.size();
+            std::vector<std::size_t> indices(population.size());
+
+            std::iota(indices.begin(), indices.end(), 0);
+
+            std::ranges::nth_element(
+                indices,
+                indices.begin() + count,
+                std::less{},
+                [&](const std::size_t i) { return population[i]; }
+            );
+
+            for (std::size_t i = 0; i < count; ++i) {
+                population[indices[i]] = deme[i];
+            }
+        }
+    };
+
+}
+
+#endif //WORST_REPLACER_H
