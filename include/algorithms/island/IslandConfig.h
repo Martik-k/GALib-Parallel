649ebf712c544c7671e22d73a10fd155929bb540
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2026-04-14 08:31:06 +0300

    refactor: minor changes in the IslandConfig.h

--- include/algorithms/island/IslandConfig.h
+++ include/algorithms/island/IslandConfig.h
@@ -6,12 +6,6 @@
 #include <cstddef>
 
 namespace galib {
-    /**
-     * @brief Configuration parameters for the Island Model Genetic Algorithm.
-     * 
-     * This struct aggregates standard GA parameters with island-specific 
-     * settings for migration and asynchronous buffering.
-     */
     struct IslandConfig {
         // Standard GA Parameters
         std::size_t population_size = 100;
@@ -20,24 +14,9 @@ namespace galib {
         double crossover_rate = 0.8;
 
         // Island Model Specific Parameters
-
-        /**
-         * @brief Number of generations between migration events.
-         */
         std::size_t migration_interval = 50;
-
-        /**
-         * @brief Number of individuals to send during each migration.
-         */
         std::size_t migration_size = 5;
         double immigration_quota = 0.1;
-
-        /**
-         * @brief Capacity of the Migration Buffer (number of batches/demes).
-         * 
-         * Determines how many incoming migrations from neighbors can be stored 
-         * before the oldest ones are overwritten.
-         */
         std::size_t buffer_capacity = 5;
     };
 }
